4e78cb133268264bb6d1dfe8815c6980d6db9408
nanigashi-zero <nanigashi08x@gmail.com>
2020-11-26 12:34:07 +0900

    Initial Commit

---
+++ FalarmTests/FalarmTests.swift
@@ -0,0 +1,34 @@
+//
+//  FalarmTests.swift
+//  FalarmTests
+//
+//  Created by 唐崎浩誠 on 2020/11/26.
+//  Copyright © 2020 唐崎浩誠. All rights reserved.
+//
+
+import XCTest
+@testable import Falarm
+
+class FalarmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
